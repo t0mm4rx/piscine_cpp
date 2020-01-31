db86f9a4da6f2adbaaa05182b7165b2f565482fb
t0mm4rx <tom.marx@hotmail.fr>
2020-01-31 14:54:35 +0100

    day01 ex04

---
+++ day01/ex04/exo04.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::string str = "HI THIS IS BRAIN";
+	std::string *ptr = &str;
+	std::string ref = *ptr;
+
+	std::cout << "String:    " << str << std::endl;
+	std::cout << "Pointer:   " << *ptr << std::endl;
+	std::cout << "Reference: " << ref << std::endl;
+}
