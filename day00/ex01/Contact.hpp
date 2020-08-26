85a070274bb3842ac34f32a21afab72322b1702c
Tom <tom.marx@hotmail.fr>
2020-08-26 11:27:27 +0200

    Fixed tabulations

--- day00/ex01/Contact.hpp
+++ day00/ex01/Contact.hpp
@@ -14,20 +14,20 @@
 class Contact
 {
 	public:
-								Contact(void);
-								~Contact(void);
-		void				print(void);
-		void				set_first_name(std::string first_name);
-		void				set_nick_name(std::string nick_name);
-		void				set_last_name(std::string last_name);
-		void				set_login(std::string login);
-		void				set_postal_address(std::string postal_address);
-		void				set_mail(std::string mail);
-		void				set_phone(std::string phone);
-		void				set_birthday(std::string birthday);
-		void				set_favorite_meal(std::string favorite_meal);
-		void				set_underwear_color(std::string underwear_color);
-		void				set_darkest_secret(std::string darkest_secret);
+					Contact(void);
+					~Contact(void);
+		void		print(void);
+		void		set_first_name(std::string first_name);
+		void		set_nick_name(std::string nick_name);
+		void		set_last_name(std::string last_name);
+		void		set_login(std::string login);
+		void		set_postal_address(std::string postal_address);
+		void		set_mail(std::string mail);
+		void		set_phone(std::string phone);
+		void		set_birthday(std::string birthday);
+		void		set_favorite_meal(std::string favorite_meal);
+		void		set_underwear_color(std::string underwear_color);
+		void		set_darkest_secret(std::string darkest_secret);
 		std::string	get_first_name(void);
 		std::string	get_last_name(void);
 		std::string	get_login(void);
@@ -45,8 +45,8 @@ class Contact
 		std::string	darkest_secret;
 };
 
-std::string	to_upper(std::string str);
-std::string	truncate(std::string src);
+std::string			to_upper(std::string str);
+std::string			truncate(std::string src);
 void				print_header(void);
 void				print_helper(void);
 void				listen_user(Contact contacts[8], int *index);
