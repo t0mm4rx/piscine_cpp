e4ed1e14a4ee983da346663d9d24be6d063942c9
Tom <tom.marx@hotmail.fr>
2020-09-15 12:57:33 +0200

    Day07

--- day07/ex00/whatever.cpp
+++ day07/ex00/whatever.cpp
@@ -18,7 +18,29 @@ template <typename T> T& max(T &a, T &b)
   return (a > b ? a : b);
 }
 
-int main(void) {
+class A
+{
+	private:
+		int _n;
+	public:
+		A(int n = 0) : _n(n)
+		{};
+		int getN(void) const
+		{
+			return (_n);
+		};
+		bool operator>(const A &other)
+		{
+			return (_n > other._n);
+		};
+		bool operator<(const A &other)
+		{
+			return (_n < other._n);
+		};
+};
+
+int main(void)
+{
   int a = 2;
   int b = 3;
   ::swap(a, b);
@@ -31,5 +53,11 @@ int main(void) {
   std::cout << "c = " << c << ", d = " << d << std::endl;
   std::cout << "min(c, d) = " << ::min(c, d) << std::endl;
   std::cout << "max(c, d) = " << ::max(c, d) << std::endl;
+  A e(42);
+  A f(21);
+  ::swap(e, f);
+  std::cout << "e = " << e.getN() << ", f = " << f.getN() << std::endl;
+  std::cout << "min(e, f) = " << ::min(e, e).getN() << std::endl;
+  std::cout << "max(e, f) = " << ::max(f, f).getN() << std::endl;
   return 0;
 }
--- day07/ex01/iter.cpp
+++ day07/ex01/iter.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 #include <string>
 
+class A
+{
+	private:
+		int _n;
+	public:
+		A(int n = 0) : _n(n)
+		{};
+		int getN(void) const
+		{
+			return (_n);
+		};
+		bool operator>(const A &other)
+		{
+			return (_n > other._n);
+		};
+		bool operator<(const A &other)
+		{
+			return (_n < other._n);
+		};
+};
+
 template <typename T> void iter(T *array, unsigned int length, void (*f)(T &elem))
 {
   for (unsigned int i = 0; i < length; i++) {
@@ -18,6 +39,11 @@ void fn2(std::string &s)
   std::cout << s << " ";
 }
 
+void fn3(A &s)
+{
+  std::cout << s.getN() << " ";
+}
+
 void printList(int *list, unsigned int length)
 {
   for (unsigned int i = 0; i < length; i++)
@@ -33,5 +59,11 @@ int main(void)
   iter(list, 7, &fn);
   printList(list, 7);
   iter(list2, 3, &fn2);
+  std::cout << std::endl;
+  A list3[3];
+  list3[0] = A(21);
+  list3[1] = A(42);
+  list3[2] = A(-42);
+  iter(list3, 3, &fn3);
   std::cout << std::endl;
 }
